0c4dd47e51ddb34d5a01b915e7faa57ed8b0097e
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-21 15:47:26 +0300

    publisher id and its getter and setter  added

--- src/main/java/com/mehdi/Spring5WebApp/model/Publisher.java
+++ src/main/java/com/mehdi/Spring5WebApp/model/Publisher.java
@@ -1,13 +1,27 @@
 package com.mehdi.Spring5WebApp.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
     private String address;
 
     public Publisher() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
