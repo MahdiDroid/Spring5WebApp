cb6526dc5f0d5431a7291ad299ff332a3146aa1f
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-21 12:51:55 +0300

    Author class  PROPERITES add

--- src/main/java/com/mehdi/Spring5WebApp/model/Author.java
+++ src/main/java/com/mehdi/Spring5WebApp/model/Author.java
@@ -1,4 +1,12 @@
 package com.mehdi.Spring5WebApp.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Author {
+
+    private String firstName;
+    private String lastName;
+
+    private Set<Book> books = new HashSet<>();
 }
