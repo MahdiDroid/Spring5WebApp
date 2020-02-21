3a156319ce84a6ef486a240e5bf53d7c6ad19d7c
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-21 12:55:26 +0300

    Book class   PROPS get and setter add

--- src/main/java/com/mehdi/Spring5WebApp/model/Book.java
+++ src/main/java/com/mehdi/Spring5WebApp/model/Book.java
@@ -9,4 +9,36 @@ public class Book {
     private String publisher;
 
     private Set<Author> authors = new HashSet<>();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 }
