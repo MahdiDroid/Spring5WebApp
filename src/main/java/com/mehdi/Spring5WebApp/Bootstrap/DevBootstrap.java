7b07bb67f696a776cc9dd30ab0ea65b751e8ff07
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-21 15:14:41 +0300

    DEvbootstrao class implemented

--- src/main/java/com/mehdi/Spring5WebApp/Bootstrap/DevBootstrap.java
+++ src/main/java/com/mehdi/Spring5WebApp/Bootstrap/DevBootstrap.java
@@ -1,9 +1,25 @@
 package com.mehdi.Spring5WebApp.Bootstrap;
 
+import com.mehdi.Spring5WebApp.Repositories.AuthorRepository;
+import com.mehdi.Spring5WebApp.Repositories.BookRepository;
 import com.mehdi.Spring5WebApp.model.Author;
 import com.mehdi.Spring5WebApp.model.Book;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 
-public class DevBootstrap {
+public class DevBootstrap extends ApplicationListener<ContextRefreshedEvent> {
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initialData();
+    }
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
 
     private void initialData(){
 
@@ -12,12 +28,18 @@ public class DevBootstrap {
         Book ddd = new Book("Domain driven design","1234","Collins");
         mehdi.getBooks().add(ddd);
         ddd.getAuthors().add(mehdi);
+        authorRepository.save(mehdi);
+        bookRepository.save(ddd);
 
         //Rod
         Author bob = new Author("bob","jessy");
         Book ttt = new Book("Test Driven   design","567","Oreilly");
         mehdi.getBooks().add(ttt);
         ddd.getAuthors().add(bob);
+
+        authorRepository.save(bob);
+        bookRepository.save(ttt);
+
 
 
     }
