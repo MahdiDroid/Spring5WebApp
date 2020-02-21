c71a4694e6e76a584dab9baff56d60898389bb01
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-21 13:07:08 +0300

    author constructor with no arg add with id and generated value

--- src/main/java/com/mehdi/Spring5WebApp/model/Author.java
+++ src/main/java/com/mehdi/Spring5WebApp/model/Author.java
@@ -1,17 +1,24 @@
 package com.mehdi.Spring5WebApp.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.HashSet;
 import java.util.Set;
 @Entity
 public class Author {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String firstName;
     private String lastName;
 
     private Set<Book> books = new HashSet<>();
+
+    public Author() {
+    }
 
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
