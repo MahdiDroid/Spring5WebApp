89cbf61e3cdf132a653d409b0eea53d3b887963e
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-21 14:36:54 +0300

    implemented part of repositories cleared

--- src/main/java/com/mehdi/Spring5WebApp/Repositories/AuthorRepository.java
+++ src/main/java/com/mehdi/Spring5WebApp/Repositories/AuthorRepository.java
@@ -7,36 +7,4 @@ import java.util.Optional;
 
 public interface AuthorRepository extends CrudRepository<Author, Long>{
 
-    @Override
-    <S extends Author> S save(S s);
-
-    @Override
-    <S extends Author> Iterable<S> saveAll(Iterable<S> iterable);
-
-    @Override
-    Optional<Author> findById(Long aLong);
-
-    @Override
-    boolean existsById(Long aLong);
-
-    @Override
-    Iterable<Author> findAll();
-
-    @Override
-    Iterable<Author> findAllById(Iterable<Long> iterable);
-
-    @Override
-    long count();
-
-    @Override
-    void deleteById(Long aLong);
-
-    @Override
-    void delete(Author author);
-
-    @Override
-    void deleteAll(Iterable<? extends Author> iterable);
-
-    @Override
-    void deleteAll();
 }
--- src/main/java/com/mehdi/Spring5WebApp/Repositories/BookRepository.java
+++ src/main/java/com/mehdi/Spring5WebApp/Repositories/BookRepository.java
@@ -1,6 +1,10 @@
 package com.mehdi.Spring5WebApp.Repositories;
 
+import com.mehdi.Spring5WebApp.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository {
+import java.util.Optional;
+
+public interface BookRepository extends CrudRepository<Book,Long> {
+
 }
