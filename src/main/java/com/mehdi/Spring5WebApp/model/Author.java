2080e9292bd009f4bc3ed7b6694ce21bdc2b6f3b
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-21 12:52:27 +0300

    Author class   PROPS get and setter  add

--- src/main/java/com/mehdi/Spring5WebApp/model/Author.java
+++ src/main/java/com/mehdi/Spring5WebApp/model/Author.java
@@ -9,4 +9,28 @@ public class Author {
     private String lastName;
 
     private Set<Book> books = new HashSet<>();
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
