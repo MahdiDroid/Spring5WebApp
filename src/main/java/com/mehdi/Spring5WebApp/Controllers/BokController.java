b3fa605f196ad9f3cc35d5766da164f731b16148
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-21 17:38:09 +0300

    BookController class implemented

--- src/main/java/com/mehdi/Spring5WebApp/Controllers/BokController.java
+++ src/main/java/com/mehdi/Spring5WebApp/Controllers/BokController.java
@@ -1,4 +1,28 @@
 package com.mehdi.Spring5WebApp.Controllers;
 
-public class BokController {
+import com.mehdi.Spring5WebApp.Repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books",bookRepository.findAll());
+        return "books";
+
+
+    }
+
+
+
 }
+
+
