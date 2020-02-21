4ec0a27bf9a5f9366f4a4d9e9ddd186e61592e12
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-21 12:54:54 +0300

    Book class   PROPS  add

--- src/main/java/com/mehdi/Spring5WebApp/model/Book.java
+++ src/main/java/com/mehdi/Spring5WebApp/model/Book.java
@@ -1,4 +1,12 @@
 package com.mehdi.Spring5WebApp.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Book {
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    private Set<Author> authors = new HashSet<>();
 }
