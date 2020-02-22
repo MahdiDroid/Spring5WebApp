2560a63806bf61b68d60788e81bd3cb830dd96d0
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 00:03:10 +0300

    getAuthors mehtod added to get the authors

--- src/main/java/com/mehdi/Spring5WebApp/Controllers/AuthorController.java
+++ src/main/java/com/mehdi/Spring5WebApp/Controllers/AuthorController.java
@@ -3,6 +3,8 @@ package com.mehdi.Spring5WebApp.Controllers;
 
 import com.mehdi.Spring5WebApp.Repositories.AuthorRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthorController {
@@ -11,5 +13,11 @@ public class AuthorController {
 
     public AuthorController(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
+    }
+
+
+    public String getAuthors(Model model ){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
     }
 }
