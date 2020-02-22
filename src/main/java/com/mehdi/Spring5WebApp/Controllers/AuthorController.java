0114d074b108ef4e7b1ac8bc2d48a0b52d8364d0
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-23 00:00:21 +0300

    AuthorRepository injected to author contoller

--- src/main/java/com/mehdi/Spring5WebApp/Controllers/AuthorController.java
+++ src/main/java/com/mehdi/Spring5WebApp/Controllers/AuthorController.java
@@ -1,8 +1,15 @@
 package com.mehdi.Spring5WebApp.Controllers;
 
 
+import com.mehdi.Spring5WebApp.Repositories.AuthorRepository;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
 }
